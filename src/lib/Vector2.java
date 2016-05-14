c2a81bff0411a4a199b338cc3de3243728bd34b9
thedevelop3r <rjm27trekkie@gmail.com>
2016-05-13 22:37:38 -0400

    tested Vector2 instance methods; directory refactor

--- src/lib/Vector2.java
+++ src/lib/Vector2.java
@@ -1,3 +1,4 @@
+package lib;
 /**
  * The Vector2 class represents a 2x1 matrix vector and provides common
  * operations on said object.
@@ -5,17 +6,20 @@
  * Some design notes:
  * <li>The cross product is returned as a float, not a 3rd dimensional vector
  * despite that it represents a vector orthogonal to any 2 input vectors.</li>
- * <li>Angles are not stored as fields; all angles are calculated when needed.</li>
+ * <li>Angles are not stored as fields; all angles are calculated when needed.
+ * </li>
  * </ul>
  * @author Robert Meade
  * @version 0.1
  */
 public class Vector2
 {
+	/** epsilon value for floating point comparison **/
+	public static final float EPSILON = .001f;
 	/** x vector component **/
-	float x;
+	private float x;
 	/** y vector component **/
-	float y;
+	private float y;
 
 	// Constructors
 	// ---------------------------------------------
@@ -39,7 +43,8 @@ public class Vector2
 	 */
 	public Vector2(float magnitude, float angle, boolean isPolar) throws IllegalArgumentException
 	{
-		if(!isPolar) throw new IllegalArgumentException(); // idiot proof
+		if (!isPolar)
+			throw new IllegalArgumentException(); // idiot proof
 		this.x = (float) Math.cos(angle);
 		this.y = (float) Math.sin(angle);
 	}
@@ -54,8 +59,72 @@ public class Vector2
 		this.y = v.y;
 	}
 
+	// Overridden methods
+	// ---------------------------------------------
+
+	@Override
+	public String toString()
+	{
+		return "[" + this.x + ", " + this.y + "]";
+	}
+
+	@Override
+	public boolean equals(Object o)
+	{
+		return o instanceof Vector2 
+				&& this.x < ((Vector2) o).x + EPSILON && this.x > ((Vector2) o).x - EPSILON
+				&& this.y < ((Vector2) o).y + EPSILON && this.y > ((Vector2) o).y - EPSILON;
+	}
+
 	// Instance methods
 	// -----------------------------------------------
+
+	/**
+	 * The copy method returns a deep copy of this vector.
+	 * @return copy of vector
+	 */
+	public Vector2 copy()
+	{
+		return new Vector2(this);
+	}
+
+	/**
+	 * The array method gets the vector and returns it as a float array.
+	 * @return float array
+	 */
+	public float[] array()
+	{
+		return new float[] { x, y };
+	}
+
+	/**
+	 * The dist method returns the distance between this vector and another
+	 * vector
+	 * @param v vector to find distance to
+	 * @return distance between vectors
+	 */
+	public float dist(Vector2 v)
+	{
+		return (float) Math.sqrt((v.x - x) * (v.x - x) + (v.y - y) * (v.y - y));
+	}
+
+	/**
+	 * The mag method returns the magnitude of the vector as a scalar.
+	 * @return magnitude of vector
+	 */
+	public float mag()
+	{
+		return (float) Math.sqrt(x * x + y * y);
+	}
+
+	/**
+	 * The magSq method returns the magnitude of the vector squared as a scalar.
+	 * @return magnitude of vector
+	 */
+	public float magSq()
+	{
+		return x * x + y * y;
+	}
 
 	/**
 	 * The angle method returns the angle formed between the x and y components
@@ -65,6 +134,29 @@ public class Vector2
 	public float angle()
 	{
 		return (float) Math.atan2(y, x);
+	}
+
+	/**
+	 * The angleBetween method returns the angle between this vector and another
+	 * in radians.
+	 * @param v vector to find angle with
+	 * @return angle between vectors
+	 */
+	public float angleBetween(Vector2 v)
+	{
+		return (float) Math.cos(dot(v) / (v.mag() * mag()));
+	}
+
+	/**
+	 * The rotate method rotates this vector by a given angle deltaTheta.
+	 * @param deltaTheta angle to rotate by
+	 */
+	public void rotate(float deltaTheta)
+	{
+		float theta = angle() + deltaTheta;
+		float mag = mag();
+		this.x = mag * (float) Math.cos(theta);
+		this.y = mag * (float) Math.sin(theta);
 	}
 
 	/**
@@ -117,7 +209,7 @@ public class Vector2
 	 */
 	public float cross(Vector2 v)
 	{
-		return this.x * v.y + this.y * v.x;
+		return x * v.y + y * v.x;
 	}
 
 	/**
@@ -127,7 +219,7 @@ public class Vector2
 	 */
 	public float dot(Vector2 v)
 	{
-		return this.x * v.x + this.y * v.y;
+		return x * v.x + y * v.y;
 	}
 
 	/**
@@ -141,14 +233,49 @@ public class Vector2
 		this.y = (float) Math.sin(theta);
 	}
 
+	/**
+	 * The lerp method computes the linear interpolation of this vector and
+	 * another vector and sets it equal to this vector
+	 * @param v vector
+	 * @param alpha alpha value
+	 */
+	public void lerp(Vector2 v, float alpha)
+	{
+		this.x *= (1 - alpha) * x + v.x * alpha;
+		this.y *= (1 - alpha) * x + v.y * alpha;
+	}
+
+	/**
+	 * The angleBetween method returns the angle between this vector and another
+	 * in radians.
+	 * @param v1 1st vector
+	 * @param v2 2nd vector
+	 * @return angle between vectors
+	 */
+	public float angleBetween(Vector2 v1, Vector2 v2)
+	{
+		return (float) Math.cos(v1.dot(v2) / (v1.mag() * v2.mag()));
+	}
+
+	/**
+	 * The rotate method returns a vector rotated from the input vector by an angle deltaTheta.
+	 * @param v vector
+	 * @param deltaTheta angle to rotate by
+	 */
+	public Vector2 rotate(Vector2 v, float deltaTheta)
+	{
+		float theta = v.angle() + deltaTheta;
+		float mag = v.mag();
+		return new Vector2(mag * (float) Math.cos(theta), mag * (float) Math.sin(theta));
+	}
+
 	// Static methods
 	// ----------------------------------------------------------
-	
+
 	/**
-	 * The cross method returns the cross product of 2 vectors as
-	 * a float. This value is indicative of a vector orthogonal to the 2 vectors
-	 * involved in its computation but is not returned as a 3rd dimensional
-	 * vector object.
+	 * The cross method returns the cross product of 2 vectors as a float. This
+	 * value is indicative of a vector orthogonal to the 2 vectors involved in
+	 * its computation but is not returned as a 3rd dimensional vector object.
 	 * @param v1 1st vector in computation
 	 * @param v2 2nd vector in computation
 	 * @return cross product as float
@@ -213,8 +340,42 @@ public class Vector2
 		return new Vector2(v.x / n, v.y / n);
 	}
 
+	/**
+	 * The normalize method converts the magnitude of this vector to 1, making
+	 * it a unit vector in its given direction.
+	 */
+	public static Vector2 normalize(Vector2 v)
+	{
+		float theta = v.angle();
+		return new Vector2((float) Math.cos(theta), (float) Math.sin(theta));
+	}
+
+	/**
+	 * The lerp method computes the linear interpolation of this vector and
+	 * another as a new vector
+	 * @param v1 1st vector
+	 * @param v2 2nd vector
+	 * @param alpha alpha value
+	 */
+	public static Vector2 lerp(Vector2 v1, Vector2 v2, float alpha)
+	{
+		return new Vector2((1 - alpha) * v1.x + v2.x * alpha, (1 - alpha) * v1.y + v2.y * alpha);
+	}
+
 	// Getters and Setters
 	// ----------------------------------------
+
+	/**
+	 * The setMag method sets the magnitude of the vector (adjusts x and y
+	 * accordingly)
+	 * @param mag magnitude
+	 */
+	public void setMag(float mag)
+	{
+		float theta = angle();
+		this.x = mag * (float) Math.cos(theta);
+		this.y = mag * (float) Math.sin(theta);
+	}
 
 	/**
 	 * The setX method sets the x component of this vector
---
+++ test/Vector2Test.java
@@ -0,0 +1,108 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import lib.Vector2;
+
+public class Vector2Test
+{
+	@Test
+	public void testNormalize()
+	{
+		Vector2 v = new Vector2(5, 5);
+		v.normalize();
+		assertEquals(v.mag(), 1, 0.1);
+		assertEquals(v.getX(), Math.sqrt(2) / 2.0, 0.001);
+		assertEquals(v.getY(), Math.sqrt(2) / 2.0, 0.001);
+	}
+	
+	@Test
+	public void testAdd()
+	{
+		Vector2 v = new Vector2(1, 1);
+		v.add(v);
+		assertEquals(v.getX(), 2, 0.001);
+		assertEquals(v.getY(), 2, 0.001);
+	}
+	
+	@Test
+	public void testSub()
+	{
+		Vector2 v = new Vector2(1, 1);
+		v.sub(v);
+		assertEquals(v.getX(), 0, 0.001);
+		assertEquals(v.getY(), 0, 0.001);
+	}
+	
+	@Test
+	public void testMult()
+	{
+		Vector2 v = new Vector2(1, 1);
+		v.mult(2);
+		assertEquals(v.getX(), 2, 0.001);
+		assertEquals(v.getY(), 2, 0.001);
+	}
+	
+	@Test
+	public void testDiv()
+	{
+		Vector2 v = new Vector2(1, 1);
+		v.div(2);
+		assertEquals(v.getX(), .5, 0.001);
+		assertEquals(v.getY(), .5, 0.001);
+	}
+	
+	@Test
+	public void testAngle()
+	{
+		Vector2 v = new Vector2(1, 1);
+		assertEquals(v.angle(), Math.PI / 4.0, .001);
+	}
+	
+	@Test
+	public void testMag()
+	{
+		Vector2 v = new Vector2(1,1);
+		assertEquals(v.mag(), Math.sqrt(2), .001);
+	}
+	
+	@Test
+	public void testMagSq()
+	{
+		Vector2 v = new Vector2(1, 1);
+		assertEquals(v.magSq(), 2, .001);
+	}
+	
+	@Test
+	public void testRotate()
+	{
+		Vector2 v = new Vector2(1, 2);
+		v.rotate((float) Math.PI / 2f);
+		assertTrue(v.equals(new Vector2(-2, 1)));
+	}
+	
+	@Test
+	public void testLerp()
+	{
+		Vector2 v1 = new Vector2(-1,1);
+		Vector2 v2 = new Vector2(1, 1);
+		v1.lerp(v2, .5f);
+		assertTrue(v1.equals(new Vector2(0f, .5f)));
+	}
+	
+	@Test
+	public void testCross()
+	{
+		Vector2 v1 = new Vector2(2, 1);
+		Vector2 v2 = new Vector2(1, 2);
+		assertEquals(v1.cross(v2), 5, 0.001);
+	}
+	
+	@Test
+	public void testDot()
+	{
+		Vector2 v1 = new Vector2(2, 1);
+		Vector2 v2 = new Vector2(1, 2);
+		assertEquals(v1.dot(v2), 4, 0.001);
+	}
+}
