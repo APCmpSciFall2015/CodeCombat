c10b10b2584d01211aaa728c6ad5c0c61b7c4f79
thedevelop3r <rjm27trekkie@gmail.com>
2016-05-16 13:18:25 -0400

    added aspects to abstract Sprite (accel, max_vel, etc.)

--- src/cc/Circle.java
+++ src/cc/Circle.java
@@ -4,5 +4,7 @@ import java.awt.Graphics;
 
 public class Circle extends Sprite
 {
+	public static final float MAX_TURNING_ANGLE = (float) Math.PI / 60f;
+	
 	public void paint(Graphics g) {}
 }
--- src/cc/Sprite.java
+++ src/cc/Sprite.java
@@ -6,11 +6,21 @@ import lib.Vector2;
 
 public abstract class Sprite
 {
+	public static final float MAX_VELOCITY = 5f;
+	
+	private World world;
 	private Vector2 position;
+	private Vector2 velocity; 
+	private Vector2 acceleration;
+	
+	// Constructors
+	// --------------------------------------------------
 	
 	public Sprite()
 	{
 		this.position = new Vector2(0, 0);
+		this.velocity = new Vector2(0, 0);
+		this.acceleration = new Vector2(0, 0);
 	}
 	
 	public Sprite(float x, float y)
@@ -18,9 +28,24 @@ public abstract class Sprite
 		this.position = new Vector2(x, y);
 	}
 	
+	public Sprite(Vector2 position, Vector2 velocity, Vector2 acceleration, World world)
+	{
+		this.position = position.copy();
+		this.velocity = velocity.copy();
+		this.acceleration = acceleration.copy();
+		this.world = world; // shallow copy; this is not accessible in subclasses directly
+	}
+	
+	// Abstract methods
+	// -----------------------------------------------------
+	
+	public abstract void update();
 	public abstract void paint(Graphics g);
 	
-	public Vector2 getPositition()
+	// Getters and setters
+	// -----------------------------------------------------
+	
+	public Vector2 getPosition()
 	{
 		return position.copy();
 	}
@@ -29,4 +54,26 @@ public abstract class Sprite
 	{
 		this.position = position.copy();
 	}
+	
+	public Vector2 getVelocity()
+	{
+		return velocity.copy();
+	}
+
+	public void setVelocity(Vector2 velocity)
+	{
+		this.velocity = velocity.copy();
+	}
+
+	public Vector2 getAcceleration()
+	{
+		return acceleration.copy();
+	}
+
+	public void setAcceleration(Vector2 acceleration)
+	{
+		this.acceleration = acceleration.copy();
+	}
+
+
 }
