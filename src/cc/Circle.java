8ebe551c643645192c8b49aee5bf2cad5d375d39
thedevelop3r <rjm27trekkie@gmail.com>
2016-05-16 12:47:02 -0400

    laid abstract framework for Sprite; made Sprite abstract

--- src/cc/Circle.java
+++ src/cc/Circle.java
@@ -1,6 +1,8 @@
 package cc;
 
+import java.awt.Graphics;
+
 public class Circle extends Sprite
 {
-
+	public void paint(Graphics g) {}
 }
--- src/cc/Sprite.java
+++ src/cc/Sprite.java
@@ -1,6 +1,32 @@
 package cc;
 
-public class Sprite
+import java.awt.Graphics;
+
+import lib.Vector2;
+
+public abstract class Sprite
 {
+	private Vector2 position;
 	
+	public Sprite()
+	{
+		this.position = new Vector2(0, 0);
+	}
+	
+	public Sprite(float x, float y)
+	{
+		this.position = new Vector2(x, y);
+	}
+	
+	public abstract void paint(Graphics g);
+	
+	public Vector2 getPositition()
+	{
+		return position.copy();
+	}
+	
+	public void setPosition(Vector2 position)
+	{
+		this.position = position.copy();
+	}
 }
