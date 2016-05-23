a3dc9b437834279dfee57a197f171d6edfdbd11a
thedevelop3r <rjm27trekkie@gmail.com>
2016-05-23 13:09:44 -0400

    merge game states

--- src/cc/World.java
+++ src/cc/World.java
@@ -8,12 +8,12 @@ import lib.Vector2;
 
 /**
  * The world class represents a plane of existence for sprites.
- * 
  * @author Josh, Matt, Jack, Abe, Robert
  * @version 0.1
  * @see Sprite
  */
-public class World {
+public class World
+{
 	/** sprites in the plane of existence **/
 	private ArrayList<Sprite> sprites;
 	/** Host applet **/
@@ -21,11 +21,10 @@ public class World {
 
 	/**
 	 * 1-Argument World Constructor
-	 * 
-	 * @param main
-	 *            host appleto
+	 * @param main host appleto
 	 */
-	public World(Main main) {
+	public World(Main main)
+	{
 		this.main = main; // shallow copy
 
 		// initialize game objects
@@ -39,8 +38,10 @@ public class World {
 	/**
 	 * The update method updates the state of the world and all its sprites.
 	 */
-	public void update() {
-		for (int i = sprites.size() - 1; i >= 0; i--) {
+	public void update()
+	{
+		for (int i = sprites.size() - 1; i >= 0; i--)
+		{
 			if (sprites.get(i).isAlive())
 				sprites.get(i).update();
 			else
@@ -48,9 +49,12 @@ public class World {
 		}
 		checkCollisions();
 	}
-	public ArrayList<Sprite> requestInView(Vector2 position, Vector2 face, float fieldOfView) {
+
+	public ArrayList<Sprite> requestInView(Vector2 position, Vector2 face, float fieldOfView)
+	{
 		ArrayList<Sprite> observed = new ArrayList<Sprite>();
-		for (Sprite s : sprites) {
+		for (Sprite s : sprites)
+		{
 			if (Math.abs(face.angle() - Vector2.sub(s.getPosition(), position).angle()) < Math.abs(fieldOfView / 2))
 				observed.add(s.copy());
 		}
@@ -60,37 +64,39 @@ public class World {
 
 	/**
 	 * The paint method paints the world and all its sprites
-	 * 
-	 * @param g
-	 *            Graphics to paint on
+	 * @param g Graphics to paint on
 	 */
-	public void paint(Graphics g) {
-		for (Sprite s : sprites) {
+	public void paint(Graphics g)
+	{
+		for (Sprite s : sprites)
+		{
 			s.paint(g);
 		}
 	}
 
 	/**
 	 * The generateObeject method adds the input sprite to the world.
-	 * 
-	 * @param s
-	 *            Sprite to add to world
+	 * @param s Sprite to add to world
 	 */
-	public void generateObject(Sprite s) {
+	public void generateObject(Sprite s)
+	{
 		sprites.add(s);
 	}
 
 	/**
 	 * The checkCollisions method manages checking for collisions on a given
 	 * update.
-	 * 
 	 * @return was there a collision?
 	 */
-	public boolean checkCollisions() {
+	public boolean checkCollisions()
+	{
 		boolean collisions = false;
-		for (int i = 0; i < sprites.size() - 1; i++) {
-			for (int j = i + 1; j < sprites.size(); j++) {
-				if (colliding(sprites.get(i), sprites.get(j))) {
+		for (int i = 0; i < sprites.size() - 1; i++)
+		{
+			for (int j = i + 1; j < sprites.size(); j++)
+			{
+				if (colliding(sprites.get(i), sprites.get(j)))
+				{
 					collisions = true;
 					sprites.get(i).collide(sprites.get(j));
 					sprites.get(j).collide(sprites.get(i));
@@ -103,14 +109,12 @@ public class World {
 	/**
 	 * The colliding method checks if 2 sprites are colliding using an
 	 * Axis-Aligned Bounding-Box
-	 * 
-	 * @param A
-	 *            1st sprite
-	 * @param B
-	 *            2nd sprite
+	 * @param A 1st sprite
+	 * @param B 2nd sprite
 	 * @return are the sprites colliding
 	 */
-	public boolean colliding(Sprite A, Sprite B) {
+	public boolean colliding(Sprite A, Sprite B)
+	{
 		// @formatter:off
 		return colliding(A.getPosition().getX() - A.getWidth() / 2, A.getPosition().getY() - A.getHeight() / 2,
 				A.getPosition().getX() + A.getWidth() / 2, A.getPosition().getY() + A.getHeight() / 2,
@@ -122,44 +126,36 @@ public class World {
 	/**
 	 * The colliding method checks if there is a collision between 2
 	 * Axis-Aligned Bounding-Boxes. !(AX < Bx || BX < Ax || AY < By || BY < Ay)
-	 * 
-	 * @param Ax
-	 *            Ax
-	 * @param Ay
-	 *            Ay
-	 * @param AX
-	 *            AX
-	 * @param AY
-	 *            AY
-	 * @param Bx
-	 *            Bx
-	 * @param By
-	 *            By
-	 * @param BX
-	 *            BX
-	 * @param BY
-	 *            By
+	 * @param Ax Ax
+	 * @param Ay Ay
+	 * @param AX AX
+	 * @param AY AY
+	 * @param Bx Bx
+	 * @param By By
+	 * @param BX BX
+	 * @param BY By
 	 * @return are the bounding boxes colliding?
 	 */
-	public boolean colliding(float Ax, float Ay, float AX, float AY, float Bx, float By, float BX, float BY) {
+	public boolean colliding(float Ax, float Ay, float AX, float AY, float Bx, float By, float BX, float BY)
+	{
 		return !(AX < Bx || BX < Ax || AY < By || BY < Ay);
 	}
 
 	/**
 	 * The getHeight method gets the height of the world
-	 * 
 	 * @return world height
 	 **/
-	public int getHeight() {
+	public int getHeight()
+	{
 		return main.getHeight();
 	}
 
 	/**
 	 * The getWidth method gets the width of the world
-	 * 
 	 * @return world width
 	 **/
-	public int getWidth() {
+	public int getWidth()
+	{
 		return main.getWidth();
 	}
 }
