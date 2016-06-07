0098bf9a6c0eb8f3c1dc92e8ef3c3d58412bf2d4
Robert <rjm27trekkie@gmail.com>
2016-06-07 12:31:06 -0400

    prototype tracking algorithm

--- src/bots/CommitBot.java
+++ src/bots/CommitBot.java
@@ -1,15 +1,22 @@
 package bots;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
 
 import lib.Vector2;
 import world.Circle;
 import world.CircleStats;
+import world.Projectile;
 import world.Sprite;
 
 public class CommitBot extends Mind
 {
 	CircleStats stats;
+	
+	
+	
 	
 	public CommitBot(CommitBot r)
 	{
@@ -28,12 +35,114 @@ public class CommitBot extends Mind
 		return new CommitBot(this);
 	}
 
+	
+	
+	private class SpriteData
+	{
+		public int lastUpdateTick = 0;
+		public Sprite s;
+		public Vector2[] positions = new Vector2[30];
+		public Vector2[] velocities = new Vector2[30];
+		
+		public SpriteData(Sprite s)
+		{
+			this.s = s;
+			positions[0] = s.getPosition();
+			velocities[0] = s.getVelocity();
+		}
+		
+		public void clearTick()
+		{
+			positions[ticks % positions.length] = null;
+			velocities[ticks % velocities.length] = null;
+		}
+		
+		public Vector2 getAverageVelocity()
+		{
+			Vector2 averagePosition = new Vector2(0,0);
+			int dataPoints = 0;
+			for(int i = 0; i < velocities.length; i++)
+			{
+				if(velocities[i] != null)
+				{
+					dataPoints++;
+					averagePosition.add(velocities[i]);
+				}
+			}
+			return averagePosition.div(dataPoints);
+		}
+		
+		public Vector2 getAveragePosition()
+		{
+			Vector2 averagePosition = new Vector2(0,0);
+			int dataPoints = 0;
+			for(int i = 0; i < positions.length; i++)
+			{
+				if(positions[i] != null)
+				{
+					dataPoints++;
+					averagePosition.add(positions[i]);
+				}
+			}
+			return averagePosition.div(dataPoints);
+		}
+		
+		public void updateData(Vector2 position, Vector2 velocity)
+		{
+			positions[ticks % positions.length] = position;
+			velocities[ticks % velocities.length] = velocity;
+			lastUpdateTick = ticks;
+		}
+		
+		
+		@Override
+		public String toString()
+		{
+			return "" + s + ": " + getAveragePosition() + ", " + getAverageVelocity();
+		}
+	}
+	
+	private HashMap<Integer, SpriteData> tracked = new HashMap<>(); 
+	
+	private int ticks = 0;
+	
+	private void updateTracked(ArrayList<Sprite> inView)
+	{
+		// clear the current datapoint
+		for(Entry<Integer, SpriteData> e : tracked.entrySet())
+		{
+			if(inView.contains(e.getValue().s)) e.getValue().clearTick();
+			if(e.getValue().s instanceof Projectile && ticks - e.getValue().lastUpdateTick > 30) tracked.remove(e.getKey());
+		}
+		
+		for(Sprite s : inView)
+		{
+			int id = s.getId();
+			if(tracked.get(s) == null)
+			{
+				tracked.put(id, new SpriteData(s));
+			}
+			else
+			{
+				tracked.get(id).updateData(s.getPosition(), s.getVelocity());
+			}
+		}
+		
+		System.out.println("Sprite Data (" + ticks + "): ");
+		for(Entry<Integer, SpriteData> e : tracked.entrySet())
+		{
+			System.out.println(e.getValue());
+		}
+		System.out.println("\n");
+	}
+	
 	@Override
 	public void think()
 	{
 		if (isAlive())
 		{
 			ArrayList<Sprite> inView = requestInView();
+			updateTracked(inView);
 			Sprite target = null;
 			for (Sprite s : inView)
 			{
@@ -74,6 +183,7 @@ public class CommitBot extends Mind
 
 			if (target != null)
 				shoot();
+			ticks++;
 		}
 		else
 		{
