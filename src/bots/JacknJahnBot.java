aa153752c8674aceaf4cfbde8aa076028e0f17f8
Matthew Jahn <mrj468@gmail.com>
2016-06-07 12:34:55 -0400

    JacknJahnBot Version 2

--- src/bots/JacknJahnBot.java
+++ src/bots/JacknJahnBot.java
@@ -28,72 +28,57 @@ public class JacknJahnBot extends Mind{
 			ArrayList<Sprite> inView = requestInView();
 			Sprite target = null;
 			Sprite shield = null;
-			Sprite mine = null;
-			Sprite obstruction = null;
 			int priorityQueue = 0;	
 
 			for (Sprite s : inView){
-				if (s instanceof Circle){
-					priorityQueue = 4;
+				if (s instanceof Circle){ //go for kill
+					priorityQueue = 3;
 				}
 			}
 			if(!super.isShielded()){
 				for (Sprite s : inView){
 					if (s instanceof Shield){
-						if(s.getPosition().dist(getPosition()) < 200){
-							priorityQueue = 3;
+						if(s.getPosition().dist(getPosition()) < 200){ //get shield
+							priorityQueue = 2;
 						}
 					}
 				}
 			}
 			for (Sprite s : inView){
 				if (s instanceof Obstacle){
-					if(s.getPosition().dist(getPosition()) < 50){
-						priorityQueue = 2;
+					if(s.getPosition().dist(getPosition()) < 50){ //avoid obstacle
+						priorityQueue = 1;
 					}
 				}
-			}
-			for (Sprite s : inView){
 				if (s instanceof Mine){
-					if(s.getPosition().dist(getPosition()) < 100){
+					if(s.getPosition().dist(getPosition()) < 150){ //avoid mine
 						priorityQueue = 1;
+					}
+				}	
+			}	
+			if (super.getVelocity().mag() < 2){ //stuck on obstacle
+				priorityQueue = 1;
+			}
+			for (Sprite s : inView){ 
+				if (s instanceof Circle){
+					if(s.getPosition().dist(getPosition()) < 100){ //ez kill
+						priorityQueue = 3;
 					}
 				}
 			}
 			switch (priorityQueue){
-			case 1://mine avoidance
-				for (Sprite s : inView){
-					if (s instanceof Mine){
-						if(s.getPosition().dist(getPosition()) < 100){
-							mine = s;
-							Vector2 direction = mine.getPosition().sub(calcEyePosition(getPosition(), getVelocity()));
-							Vector2 left = new Vector2(1, getVelocity().angle() + Circle.FOV / 2, true);
-							Vector2 right = new Vector2(1, getVelocity().angle() - Circle.FOV / 2, true);
-							turn((float) -(Math.acos(direction.dot(left) / (direction.mag())) - Math.acos(direction.dot(right) / (direction.mag()))));
-						}
-					}
-				}
-			case 2://obstacle avoidance
-				for (Sprite s : inView){
-					if (s instanceof Obstacle){
-						if(s.getPosition().dist(getPosition()) < 50){
-							obstruction = s;
-							Vector2 direction = obstruction.getPosition().sub(calcEyePosition(getPosition(), getVelocity()));
-							Vector2 left = new Vector2(1, getVelocity().angle() + Circle.FOV / 2, true);
-							Vector2 right = new Vector2(1, getVelocity().angle() - Circle.FOV / 2, true);
-							turn((float) -(Math.acos(direction.dot(left) / (direction.mag())) - Math.acos(direction.dot(right) / (direction.mag()))));
-						}
-					}
-				}
+			case 1://avoidance
+				turn((float) Circle.MAX_TURNING_ANGLE);
 				break;
-			case 3://get shield
-				for (Sprite s : inView){
-					if (s instanceof Shield){ //Shield Getting
-						if(shield != null && s.getPosition().dist(getPosition()) < shield.getPosition().dist(getPosition())){
-							shield = s;
+			
+			case 2://get shield
+				for (Sprite b : inView){
+					if (b instanceof Shield){ 
+						if(shield != null && b.getPosition().dist(getPosition()) < shield.getPosition().dist(getPosition())){
+							shield = b;
 						}
 						else if (shield == null);{
-							shield = s;
+							shield = b;
 						}
 						if(target != null){
 							Vector2 direction = shield.getPosition().sub(calcEyePosition(getPosition(), getVelocity()));
@@ -104,14 +89,14 @@ public class JacknJahnBot extends Mind{
 					}
 				}
 				break;
-			case 4: //murder
-				for (Sprite s : inView){
-					if (s instanceof Circle){ //Hunter Killer
-						if(target != null && s.getPosition().dist(getPosition()) < target.getPosition().dist(getPosition())){
-							target = s;
+			case 3: //murder
+				for (Sprite c : inView){
+					if (c instanceof Circle){ 
+						if(target != null && c.getPosition().dist(getPosition()) < target.getPosition().dist(getPosition())){
+							target = c;
 						}
 						else if (target == null);{
-							target = s;
+							target = c;
 						}
 						if(target != null){
 							Vector2 direction = target.getPosition().sub(calcEyePosition(getVelocity(), getPosition()));
@@ -137,4 +122,4 @@ public class JacknJahnBot extends Mind{
 		return "JacknJahnBot";
 	}
 }
-	
+
